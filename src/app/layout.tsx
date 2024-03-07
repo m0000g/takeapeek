d699527d37e8a8fa9477283a36f7e56837b85be9
m0000g <m0000g@gmail.com>
2024-03-07 11:21:07 -0600

    Issue with refresh solved, removed extra fonts and component, adjusted configuration according to the READMA.md doc, also remove unit test, extra configurations for that vitest and used a clean package.json file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
-import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <main>{children}</main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-'use client';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 
-export default function Page() {
+export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <div>
-          <h1>Home</h1>
-          <Button size='lg' variant='destructive'>
-            Click me!
-          </Button>
-        </div>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Get started by editings&nbsp;
+          Get started by editing&nbsp;
           <code className='font-mono font-bold'>src/app/page.tsx</code>
         </p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
@@ -23,7 +15,8 @@ export default function Page() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            By <Image src='/vercel.svg' alt='Vercel Logo' className='dark:invert' width={100} height={24} priority />
+            By
+            <Image src='/vercel.svg' alt='Vercel Logo' className='dark:invert' width={100} height={24} priority />
           </a>
         </div>
       </div>
@@ -106,6 +99,6 @@ export default function Page() {
           </p>
         </a>
       </div>
-    </div>
+    </main>
   );
 }
